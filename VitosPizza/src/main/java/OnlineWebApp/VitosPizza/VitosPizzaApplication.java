6b7464156b159c0c8d73b78e72d3acd5b94d4cd3
Lasith Rajakarunaratne <Lasith.Rajakarunaratne@synergenhealth.com>
2024-07-05 06:55:24 -0500

    Add initial files

---
+++ VitosPizza/src/main/java/OnlineWebApp/VitosPizza/VitosPizzaApplication.java
@@ -0,0 +1,13 @@
+package OnlineWebApp.VitosPizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitosPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitosPizzaApplication.class, args);
+	}
+
+}
---
+++ VitosPizza/src/test/java/OnlineWebApp/VitosPizza/VitosPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package OnlineWebApp.VitosPizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitosPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
